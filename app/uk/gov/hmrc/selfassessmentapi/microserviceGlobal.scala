83b7bf96f7774509129096da191caea94186f5bd
Krzysztof Hycnar <krzysztof.hycnar@digital.hmrc.gov.uk>
2016-04-19 14:37:30 +0100

    MTDSA-189 - Implement auth filter.

--- app/uk/gov/hmrc/selfassessmentapi/microserviceGlobal.scala
+++ app/uk/gov/hmrc/selfassessmentapi/microserviceGlobal.scala
@@ -1,14 +1,31 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.selfassessmentapi
 
 import com.typesafe.config.Config
 import play.api.{Application, Configuration, Play}
 import uk.gov.hmrc.play.audit.filters.AuditFilter
-import uk.gov.hmrc.play.auth.controllers.AuthParamsControllerConfig
+import uk.gov.hmrc.play.auth.controllers.{AuthConfig, AuthParamsControllerConfig}
 import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode}
 import uk.gov.hmrc.play.http.logging.filters.LoggingFilter
 import uk.gov.hmrc.play.microservice.bootstrap.DefaultMicroserviceGlobal
 import uk.gov.hmrc.play.auth.microservice.filters.AuthorisationFilter
 import net.ceedubs.ficus.Ficus._
+import uk.gov.hmrc.play.auth.microservice.connectors.{AccountId, HttpVerb, Regime, ResourceToAuthorise}
 
 
 object ControllerConfiguration extends ControllerConfig {
@@ -31,6 +48,22 @@ object MicroserviceLoggingFilter extends LoggingFilter {
 object MicroserviceAuthFilter extends AuthorisationFilter {
   override lazy val authParamsConfig = AuthParamsControllerConfiguration
   override lazy val authConnector = MicroserviceAuthConnector
+
+  override def extractResource(pathString: String, verb: HttpVerb, authConfig: AuthConfig): Option[ResourceToAuthorise] = {
+    authConfig.mode match {
+      case "identity" => extractIdentityResource(pathString, verb, authConfig)
+      case "passcode" => super.extractResource(pathString, verb, authConfig)
+    }
+  }
+
+  private def extractIdentityResource(pathString: String, verb: HttpVerb, authConfig: AuthConfig): Option[ResourceToAuthorise] = {
+    pathString match {
+      case authConfig.pattern(utr) =>
+        Some(ResourceToAuthorise(verb, Regime("sa"), AccountId(utr)))
+      case _ => None
+    }
+  }
+
   override def controllerNeedsAuth(controllerName: String): Boolean = ControllerConfiguration.paramsForController(controllerName).needsAuth
 }
 
