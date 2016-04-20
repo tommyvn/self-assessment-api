17d2d16f287fb8e4bb9eb3f4f017fb0dc30ddf25
saurabh arora <saurabh.arora@digital.hmrc.gov.uk>
2016-04-20 17:24:40 +0100

    [MTDSA-189] [SA] the name AuthFilterSpec leaks the implementation of Authorisation; functional test name must specify behaviour under test

--- it/uk/gov/hmrc/selfassessmentapi/AuthorisationSpec.scala
+++ it/uk/gov/hmrc/selfassessmentapi/AuthorisationSpec.scala
@@ -2,7 +2,7 @@ package uk.gov.hmrc.selfassessmentapi
 
 import uk.gov.hmrc.support.BaseFunctionalSpec
 
-class AuthFilterSpec extends BaseFunctionalSpec {
+class AuthorisationSpec extends BaseFunctionalSpec {
 
   "if the user is not authorised for the resource they" should {
     "receive 401" in {
