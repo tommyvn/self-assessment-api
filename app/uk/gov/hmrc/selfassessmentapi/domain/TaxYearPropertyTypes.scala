7c9cb14fba696dc141aa3c6b71ed9e96d7eb2b33
Krzysztof Hycnar <krzysztof.hycnar@digital.hmrc.gov.uk>
2016-06-13 17:04:27 +0100

    [MTDSA-261] - [KH|RK] Collect taxpayer's Student Loan details - added documentation.

--- app/uk/gov/hmrc/selfassessmentapi/domain/TaxYearPropertyTypes.scala
+++ app/uk/gov/hmrc/selfassessmentapi/domain/TaxYearPropertyTypes.scala
@@ -19,10 +19,11 @@ package uk.gov.hmrc.selfassessmentapi.domain
 import uk.gov.hmrc.selfassessmentapi.domain.blindperson.BlindPersons
 import uk.gov.hmrc.selfassessmentapi.domain.charitablegiving.CharitableGivings
 import uk.gov.hmrc.selfassessmentapi.domain.pensioncontribution.PensionContributions
+import uk.gov.hmrc.selfassessmentapi.domain.studentsloan.StudentLoans
 import uk.gov.hmrc.selfassessmentapi.domain.taxrefundedorsetoff.TaxRefundedOrSetOffs
 
 object TaxYearPropertyTypes {
-  val types = Seq(PensionContributions, CharitableGivings, BlindPersons, TaxRefundedOrSetOffs)
+  val types = Seq(PensionContributions, CharitableGivings, BlindPersons, TaxRefundedOrSetOffs, StudentLoans)
   private val typesByName = types.map(x => x.name -> x).toMap
   def fromName(name: String): Option[TaxYearPropertyType] = typesByName.get(name)
 }
