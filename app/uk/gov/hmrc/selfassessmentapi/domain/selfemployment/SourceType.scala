76b2a66d232ee7ee1210f16a25d1124f3e161303
Dario Abdulrehman <dario.rehman@gmail.com>
2016-07-25 13:37:51 +0100

    [MTDSA-428] Improve JSON field descriptions for source of income

--- app/uk/gov/hmrc/selfassessmentapi/domain/selfemployment/SourceType.scala
+++ app/uk/gov/hmrc/selfassessmentapi/domain/selfemployment/SourceType.scala
@@ -34,21 +34,27 @@ object SourceType {
     override val fieldDescriptions = Seq(
       FullFieldDescription(name, "commencementDate", "Date", "2016-01-01", "Date in yyyy-dd-mm format"),
       FullFieldDescription(name, "allowances", "Object", "", "Allowances claimed for this self-employment", optional = true),
-      PositiveMonetaryFieldDescription(name, "annualInvestmentAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "capitalAllowanceMainPool", optional = true),
-      PositiveMonetaryFieldDescription(name, "capitalAllowanceSpecialRatePool", optional = true),
-      PositiveMonetaryFieldDescription(name, "restrictedCapitalAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "businessPremisesRenovationAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "enhancedCapitalAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "allowancesOnSales", optional = true),
+      PositiveMonetaryFieldDescription(name, "annualInvestmentAllowance",
+        """If you bought equipment (but not cars) on or after 6 April 2014, you can claim Annual Investment Allowance (AIA). Up to 31 December 2015 the maximum annual amount of AIA was £500,000.
+          |From 1 January 2016 the maximum annual amount of AIA is £200,000
+          |If you use the equipment for both business and private use, you’ll need to reduce the Annual Investment Allowance (AIA) you claim by the private use proportion""".stripMargin, optional = true),
+      PositiveMonetaryFieldDescription(name, "capitalAllowanceMainPool", "Capital allowances at 18% on equipment, including cars with lower CO2 emissions", optional = true),
+      PositiveMonetaryFieldDescription(name, "capitalAllowanceSpecialRatePool", "Capital allowances at 8% on equipment, including cars with higher CO2 emissions", optional = true),
+      PositiveMonetaryFieldDescription(name, "restrictedCapitalAllowance", "Restricted capital allowances for cars costing more than £12,000 – if bought before 6 April 2009", optional = true),
+      PositiveMonetaryFieldDescription(name, "businessPremisesRenovationAllowance", "Business Premises Renovation Allowance (BPRA) (Assisted Areas only)", optional = true),
+      PositiveMonetaryFieldDescription(name, "enhancedCapitalAllowance", "100% and other enhanced capital allowances", optional = true),
+      PositiveMonetaryFieldDescription(name, "allowancesOnSales", "Allowances on sale or cessation of business use (where you have disposed of assets for less than their tax value)", optional = true),
       FullFieldDescription(name, "adjustments", "Object", "", "Adjustments for this self-employment", optional = true),
-      PositiveMonetaryFieldDescription(name, "includedNonTaxableProfits", optional = true),
-      MonetaryFieldDescription(name, "basisAdjustment", optional = true),
-      PositiveMonetaryFieldDescription(name, "overlapReliefUsed", optional = true),
-      PositiveMonetaryFieldDescription(name, "accountingAdjustment", optional = true),
-      MonetaryFieldDescription(name, "averagingAdjustment", optional = true),
-      PositiveMonetaryFieldDescription(name, "lossBroughtForward", optional = true),
-      PositiveMonetaryFieldDescription(name, "outstandingBusinessIncome", optional = true)
+      PositiveMonetaryFieldDescription(name, "includedNonTaxableProfits", "Income, receipts and other profits included in business income or expenses but not taxable as business profits", optional = true),
+      MonetaryFieldDescription(name, "basisAdjustment",
+        """You pay tax on the profits of your basis period for the tax year.
+          |When you’ve been in business for a couple of years, the basis period is usually the 12-month accounting period.
+          |Different rules apply when you start or cease a business or if you change accounting date""".stripMargin, optional = true),
+      PositiveMonetaryFieldDescription(name, "overlapReliefUsed", "Overlap relief used this year", optional = true),
+      PositiveMonetaryFieldDescription(name, "accountingAdjustment", "Adjustment for change of accounting practice", optional = true),
+      MonetaryFieldDescription(name, "averagingAdjustment", "Averaging adjustment (only for farmers, market gardeners and creators of literary or artistic works)", optional = true),
+      PositiveMonetaryFieldDescription(name, "lossBroughtForward", "Loss brought forward from earlier years set off against\n\nthis year’s profits", optional = true),
+      PositiveMonetaryFieldDescription(name, "outstandingBusinessIncome", "Any other business income, such as rebates received, and non arm’s length reverse premiums", optional = true)
     )
   }
 
--- app/uk/gov/hmrc/selfassessmentapi/domain/ukproperty/SourceType.scala
+++ app/uk/gov/hmrc/selfassessmentapi/domain/ukproperty/SourceType.scala
@@ -25,21 +25,41 @@ object SourceType {
 
   case object UKProperties extends SourceType {
     override val name = "uk-properties"
+
     override def example(sourceId: Option[SourceId] = None): JsValue = toJson(UKProperty.example(sourceId))
-    override val summaryTypes : Set[SummaryType] = Set(Incomes, Expenses, TaxesPaid, BalancingCharges, PrivateUseAdjustments)
+
+    override val summaryTypes: Set[SummaryType] = Set(Incomes, Expenses, TaxesPaid, BalancingCharges, PrivateUseAdjustments)
     override val title = "Sample UK property"
 
     override def description(action: String) = s"$action a UK property"
 
     override val fieldDescriptions = Seq(
       FullFieldDescription(name, "allowances", "Object", "", "Allowances claimed for this property", optional = true),
-      PositiveMonetaryFieldDescription(name, "annualInvestmentAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "businessPremisesRenovationAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "otherCapitalAllowance", optional = true),
-      PositiveMonetaryFieldDescription(name, "wearAndTearAllowance", optional = true),
+      PositiveMonetaryFieldDescription(name, "annualInvestmentAllowance",
+        """You can claim Annual Investment Allowance
+          |(AIA) if you bought equipment (but not cars)
+          |during the year. You can claim AIA up to
+          |a maximum annual amount. You can’t claim AIA
+          |for expenditure on equipment and other items for
+          |use in a dwelling house""".stripMargin, optional = true),
+      PositiveMonetaryFieldDescription(name, "businessPremisesRenovationAllowance",
+        """You may be able to claim 100% BPRA for the
+          |cost of renovating or repairing business premises.
+          |To qualify, it must be in an Assisted Area
+          |and unused for at least 1 year before the work to
+          |bring them back into business use started""".stripMargin, optional = true),
+      PositiveMonetaryFieldDescription(name, "otherCapitalAllowance",
+        """The type of capital allowance and amount that
+          |you can claim will depend on the cost, type of
+          |asset and other circumstances. For example,
+          |you can only claim capital allowances for
+          |furniture and fixtures or other equipment for use
+          |in a dwelling house if it qualifies as a Furnished
+          |Holiday Letting (FHL)""".stripMargin, optional = true),
+      PositiveMonetaryFieldDescription(name, "wearAndTearAllowance", "10% wear and tear allowance", optional = true),
       FullFieldDescription(name, "adjustments", "Object", "", "Adjustments for this property", optional = true),
-      PositiveMonetaryFieldDescription(name, "lossBroughtForward"),
-      PositiveMonetaryFieldDescription(name, "rentARoomRelief", optional = true)
+      PositiveMonetaryFieldDescription(name, "lossBroughtForward", "Loss brought forward used against this year’s profits"),
+      PositiveMonetaryFieldDescription(name, "rentARoomRelief", "Rent a Room exempt amount", optional = true)
     )
   }
 
